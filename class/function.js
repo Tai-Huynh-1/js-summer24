398034e48a7beab42e90bd686463884c02fd3949
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2024-08-08 14:43:33 -0700

    functions, stack, heap, return, control flow

--- class/function.js
+++ class/function.js
@@ -9,7 +9,7 @@ function greet(salutation) {
 
 // copy-by-value vs copy-by-reference value
 function addFive(num) {
-	num + 5;
+	num = num + 5;
 }
 
 function modifyToFive(array) {
@@ -18,11 +18,11 @@ function modifyToFive(array) {
 
 let a = 5;
 addFive(a); // passing in 'a' to function
-console.log(a); // 10, 5
+// console.log(a); // 10, 5
 
 let arrA = [200]; // 'aabbcc'
 modifyToFive(arrA); // passing in arrA to function
-console.log(arrA); // [5], [5]
+// console.log(arrA); // [5], [5]
 
 // C (what is an array in memory and how is it represented as binary?)
 // int array of size 5, each int is 1 byte (8-bit) [1, 2, 3, 4, 5]
@@ -32,8 +32,81 @@ console.log(arrA); // [5], [5]
 
 let num = 5;
 addFive(num);
-console.log(num); // 5
+// console.log(num); // 5
 
 let array = [200];
 modifyToFive(array);
-console.log(array); // [5]
+// console.log(array); // [5]
+
+function greet2() {
+	console.log("greeting 2 you");
+}
+// greet2();
+
+// in math:
+// function f(x) {
+// 	return x + 1;
+// }
+// f(5); // 6
+
+function printName(name) {
+	const firstLetter = name.charAt(0); // name[0]
+
+	if (firstLetter === "A") {
+		console.log("name starts with letter A");
+	} else if (firstLetter === "B") {
+		console.log("name starts with letter B");
+	} else if (firstLetter === "C") {
+		console.log("name starts with letter C");
+	} else {
+		console.log("name starts with something else");
+	}
+
+	console.log("func ends");
+}
+
+function altPrintName(name) {
+	const firstLetter = name.charAt(0); // name[0]
+
+	if (firstLetter !== "A") {
+		console.log("name starts with something else");
+		console.log("func ends");
+		return;
+	}
+
+	console.log("name starts with letter A");
+	console.log("func ends");
+}
+
+// altPrintName("Zeta");
+
+// return a value
+function add(num) {
+	let b = [1, 2]; // '10000'
+	let c = 1;
+	return num + 5;
+}
+
+// const returnedValue = add(6);
+// console.log(add(6));
+
+// call stack
+function print(val) {
+	console.log(val);
+}
+const myLetterA = "A";
+const myLetterB = "B";
+
+console.log(myLetterA);
+print(myLetterB);
+
+print(add(5));
+
+console.log("hello" + " " + "world");
+console.log("hello".concat(" world"));
+let i = 0;
+let val = 100;
+console.log("index is: " + i + ", value is: " + val); // <----- string concatenation with "+"
+console.log(`index is: ${i}, value is: ${val} `); // <------ template string literals
+console.log("index is: ".concat(i, ", value is: ", val)); // .concat method
+console.log("hello".includes());
