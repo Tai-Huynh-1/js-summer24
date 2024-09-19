0d2a51b3d51cf0b46579c1a0a266c89b7b1cbc5a
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2024-09-19 15:07:00 -0700

    refs and lifting state up

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,11 +2,12 @@ import ReactDOM from "react-dom/client";
 import List from "./components/List";
 import Accordion from "./components/Accordion";
 import SignUp from "./components/SignUp";
+import Calculator from "./components/Calculator";
 // import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<div>
-		<SignUp />
+		<Calculator />
 	</div>
 );
